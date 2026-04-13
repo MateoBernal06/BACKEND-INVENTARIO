ef6fd08ec147dd53405249d338480abecb881be7
MateoBernal06 <133398097+MateoBernal06@users.noreply.github.com>
2026-04-12 20:08:10 -0500

    Envio de rol

--- src/database/system.database.js
+++ src/database/system.database.js
@@ -3,7 +3,7 @@ import { supabase } from "../config/database.js";
 const login = async (rol, email) => {
     const { data, error } = await supabase
         .from(rol)
-        .select("id, name, surname, address, email, phone, password")
+        .select("id, name, surname, address, email, phone, password, role")
         .eq("email", email);
 
     if (error) {
